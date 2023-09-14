94870f9f6e5d3517bf1b09e49500800be5ee6bbb
TamannaJahan55 <tamannajahan.mis049@gmail.com>
2023-09-15 03:11:13 +0600

    components, courses jsx file, json file created and data fetched

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Courses from './Components/Courses/Courses'
+import Header from './Components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      
+      <Header></Header>
+      <Courses></Courses>
+      
+    </>
+  )
+}
+
+export default App
